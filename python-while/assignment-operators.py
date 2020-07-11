7fc65f49e9347519d6fff19626935bfaa845c8da
Dixit Pathak <dixitpathak008@gmail.com>
2020-07-12 01:40:34 +0530

    Iterate through code blocks using while statement

---
+++ python-while/assignment-operators.py
@@ -0,0 +1,14 @@
+# count = 0
+# while count != 5:
+#     count += 1
+#     print(count)
+
+# count = 0
+# while count <= 20:
+#     count += 3
+#     print(count)
+
+count = 20
+while count >= 0:
+    count -= 3
+    print(count)
---
+++ python-while/while-statement.py
@@ -0,0 +1,52 @@
+import random 
+
+# roll = 0
+# count = 0
+
+# while roll != 5:
+#   count = count + 1
+#   roll = random.randint(1, 5)
+#   print(roll)
+
+# print(f'It took {count} rolls to roll a 5!')
+
+
+# roll = 0
+# count = 0
+
+# print('First person to roll a 5 wins!')
+# while roll != 5:
+  
+#   name = input('Enter a name, or \'q\' to quit:  ' )
+#   if name == 'q':
+#     break
+  
+#   count = count + 1
+#   roll = random.randint(1, 5)
+#   print(f'{name} rolled {roll}')
+# else:
+#     print(f'{name} Wins!!!')
+
+# print(f'You rolled the dice {count} times.')
+
+
+roll = 0
+count = 0   
+
+print('First person to roll a 5 wins!')
+while roll != 5:
+  name = input('Enter a name, or \'q\' to quit:  ' )
+
+  if name.strip() == '':
+    continue
+
+  if name.strip() == 'q':
+      break
+  
+  count = count + 1
+  roll = random.randint(1, 5)
+  print(f'{name} rolled {roll}')
+else:
+    print(f'{name} Wins!!!')
+
+print(f'You rolled the dice {count} times.')

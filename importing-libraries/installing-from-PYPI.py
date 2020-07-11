22eae50a689491db27d773d02ccff74139c3c904
Dixit Pathak <dixitpathak008@gmail.com>
2020-07-12 00:03:31 +0530

    Import standard library modules to add features

---
+++ importing-libraries/installing-from-PYPI.py
@@ -0,0 +1,8 @@
+"""
+Run the following command in the terminal to install emoji library from PYPI
+py -m pip install emoji
+"""
+
+import emoji
+message = emoji.emojize('Howdy :sun_with_face:')
+print(message)
